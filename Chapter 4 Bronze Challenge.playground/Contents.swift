de27bb21b4b03a2b4249dc33f50f544ecdec30be
Jennifer Foster <jennifer.foster@d25s70nsgq18m.oc.edu>
2016-09-01 16:50:35 -0500

    Assignment 1 Commit

---
+++ Chapter 4 Bronze Challenge.playground/Contents.swift
@@ -0,0 +1,6 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// Binary representation of -1 as an 8-bit signed integer: 10000001
+// 10000001 interpreted as an 8-bit unsigned integer: 129
---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+
+let townName = "Knowhere"
+var unemploymentRate: Double = 0.05
+let townDescription = "\(townName) has a population of \(population), an unemployment rate of \(unemploymentRate * 100)%, and \(numberOfStoplights) stoplights."
+
+print(townDescription)
+
+
+
+
+
+
+
